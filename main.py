aed98a980430757aa6513663e3dff05d4c5d21f3
Thomas Tramberger <thomas.tramberger@gmail.com>
2021-07-14 20:56:41 +0200

    fixed

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from numpy.core.shape_base import hstack
 from scipy.spatial.transform import Rotation as R
 
 
-IMGFOLDER = Path(r'/images')
+IMGFOLDER = Path(r'/home/thomas/PoseTransform/images/')
 POSFILE = Path(r'pose_test.csv')
-OUTFOLDER = Path(r'/imagesOut')
+OUTFOLDER = Path(r'/home/thomas/PoseTransform/imagesOut/')
 
 MODE = 'FIRST' #other options: 'first', ...
 
 STEP = 0.001 #mm
 
-K = np.array([[3707.172386, 0.0, 1270.210342], [0.0, 3700.81825, 1058.7798870000001], [0.0, 0.0, 1.0]])
+K = np.array([[3707.172386, 0.0, 1270.210342,0.0], [0.0, 3700.81825, 1058.7798870000001, 0.0], [0.0, 0.0, 1.0, 0.0]])
 D = np.array([-0.188644, 0.145316, -0.001098, -0.000444, 0.0])
 
 def main():
@@ -80,20 +80,11 @@ def getInitialPose(poses):
 
 def rectifyImages(images, relPoses):
     ''' Rectify the images with the provided relativePoses'''
-    # extend camera matrix
-    K_4x4 = np.vstack((K,np.array([0,0,0])))
-    K_4x4 = np.hstack((K_4x4,np.array([[0],[0],[0],[1]])))
 
-
-    K_1 = np.linalg.inv(K)
-    K_1[2,2] = 0
-    K_1 = np.vstack((K_1,np.array([0,0,1])))
-    print(K_1)
-
-    K_2 = np.hstack((K,np.array([[0],[0],[0]])))
-    print(K_2)
-    print()
-
+    # K inverse
+    Kinv = np.zeros((4,3))
+    Kinv[:3,:3] = np.linalg.inv(K[:3,:3])#*3707.172386
+    Kinv[-1,:] = [0, 0, 1]
 
     #for all images
     i = 0
@@ -107,26 +98,14 @@ def rectifyImages(images, relPoses):
         relR = relPoses[i][0:3,0:3]
         relT = relPoses[i][0:3,3]
 
-        relRext = np.hstack((relR, np.array([[0],[0],[0]])))
-        relRext = np.vstack((relRext, np.array([0,0,0,1])))
+        R = np.hstack((relR, np.array([[0],[0],[0]])))
+        R = np.vstack((R, np.array([0,0,0,1])))
         
-        relText = np.identity(4)
-        relText[0:3,3] = relT
+        T = np.identity(4)
+        T[0:3,3] = relT
 
-        #normal = relR @ np.array([0,0,1])
-        #origin = relR @ np.array([[0],[0],[0]]) + relT
-        #d = 1 / (normal @ origin)
+        H = np.linalg.multi_dot([K, R, T, Kinv])
 
-        #H_eucl = relR - (relT*normal/0.02).reshape((3,1))#relT.reshape((3,1))#(relT*normal/0.02)
-        #H_eucl = np.array([[1,0,0.05],[0,1,0], [0,0,1]])
-
-        #H = K_4x4 @ (relText @ (relRext @ np.linalg.inv(K_4x4)))
-        #H = K_4x4 @ (relPoses[i] @ np.linalg.inv(K_4x4))
-        #H = H[0:3,0:3]
-        #H = H[0:3,0:3] - H[0:3,3]
-
-        H = K_2 @ (relText @ (relRext @ K_1))
-        print(H)
         #transfrom image and save
         img = cv2.warpPerspective(img, H, (img.shape[1], img.shape[0]))
         cv2.imwrite(str(OUTFOLDER.joinpath(imgPath.name)), img)
